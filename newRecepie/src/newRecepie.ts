0112f50d90df38f60dfef97ef0873efe662df3cc
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-01-09 19:22:11 +0100

    Almost done with client side

--- newRecepie/src/newRecepie.ts
+++ newRecepie/src/newRecepie.ts
@@ -1,2 +1,46 @@
 /// <reference path="../../lib/keyword"/>
 window.onload =  activateKeyWordText();
+
+class Recepie
+{
+	name:string ="";
+	ingredients:Array<[string,string]> = [];
+	instructions:string = "";
+	tags:Array<string> [];
+	rating:number;
+	comment:string = "";
+
+	constructor()
+	{
+		this.name = "";
+		this.ingredients = [];
+		this.instructions = "";
+		this.tags = [];
+		this.rating = -1;
+		this.comment = "";
+	}
+}
+
+function newRecepie()
+{
+	let rec:Recepie = new Recepie();
+
+	rec.name = document.getElementById("name").value;
+
+	let ingredients:any = document.getElementById("kC_ingredients").children;
+	for(let i = 0; i < ingredients.length; i++)
+	{
+		rec.ingredients.push(ingredients[i].innerHTML);
+	}
+	alert(rec.ingredients);
+
+	let tags:any = document.getElementById("kC_tags").children; 
+	for(let i = 0; i < tags.length; i++)
+	{
+		rec.tags.push(tags[i].innerHTML);
+	}
+	alert(rec.tags);
+	
+	rec.rating = Number(document.getElementById('rating').value);
+	alert(rec.rating);
+}
